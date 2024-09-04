c6b2f1abb8384e124b0db0fdde1efc57a96a8faa
KINGSMITH97 <kingsleyappiah2143@gmail.com>
2024-09-04 09:24:49 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript to show an alert when the button is clicked
+document.getElementById('alertButton').addEventListener('click', function() {
+    alert('Hello! You clicked the button.');
+});
